dda9faab3cb0b959bf70b62706b32f55470e4ee7
semoussoren <soussoren7@gmail.com>
2023-11-20 16:45:37 +0100

    docstrings added

--- src/smallangle/smallangle.py
+++ src/smallangle/smallangle.py
@@ -12,6 +12,11 @@ def cmd_group():
 @cmd_group.command()
 @click.option("-n", "--number", default=10)
 def sin(number):
+    """calculating the values of sin(x) for n amount of number equally distributed between 0 and 2pi
+
+    Args:
+        number (integer): 0 - 2pi equally devided by this amount 
+    """
     x = np.linspace(0, 2 * pi, number)
     df = pd.DataFrame({"x": x, "sin (x)": np.sin(x)})
     print(df)
@@ -21,6 +26,11 @@ def sin(number):
 @cmd_group.command()
 @click.option("-n", "--number", default=10)
 def tan(number):
+    """calculating the values of tan(x) for n amount of number equally distributed between 0 and 2pi
+
+    Args:
+        number (integer): 0 - 2pi equally devided by this amount 
+    """
     x = np.linspace(0, 2 * pi, number)
     df = pd.DataFrame({"x": x, "tan (x)": np.tan(x)})
     print(df)
